89a49fa6375c03171f5a25c534dec2ff8d00db20
charlysan <chrlysn0@gmail.com>
2018-01-28 17:52:10 -0300

    - Added Hid methods - Better CRC calculation method - Replaced hardcoded values with constants

--- rfidhid/core.py
+++ rfidhid/core.py
@@ -27,16 +27,29 @@ import usb.control
 
 class RfidHid(object):
     r"""Main object used to communicate with the device""" 
-    DEFAULT_VID = 0xffff
-    DEFAULT_PID = 0x0035
-    HID_REPORT_DESCRIPTOR_SIZE = 28
-    CLASS_TYPE_REPORT = 0x22
-    SET_REPORT = 0x09
-    GET_REPORT = 0x01
-    CRC_WRITE_INIT_VALUE = 0xb9
-    BUFFER_SIZE = 256
+    DEVICE_DEFAULT_VID = 0xffff
+    DEVICE_DEFAULT_PID = 0x0035
+    DEVICE_HID_REPORT_DESCRIPTOR_SIZE = 28
+    DEVICE_HID_INTERFACE_0 = 0x00
 
-    def __init__(self, vendor_id=DEFAULT_VID, product_id=DEFAULT_PID):
+    CMD_READ_TAG = 0x25
+    CMD_WRITE_TAG = 0x21
+    CMD_BEEP = 0x89
+    STX_POS = 0x08
+    STX = 0xaa
+    ETX = 0xbb
+
+    HID_REPORT_TYPE_FEATURE = 0x03
+    HID_REQUEST_HOST_TO_DEVICE_CLASS_INTERFACE = 0x21
+    HID_REQUEST_DEVICE_TO_HOST_CLASS_INTERFACE = 0xa1
+    HID_SET_REPORT = 0x09
+    HID_GET_REPORT = 0x01
+    CLASS_TYPE_REPORT = 0x22
+
+    BUFFER_SIZE = 256
+    
+
+    def __init__(self, vendor_id=DEVICE_DEFAULT_VID, product_id=DEVICE_DEFAULT_PID):
         r"""Open the device using vid and pid
 
         If no arguments are supplied then the default vid and pid will be used.
@@ -46,6 +59,47 @@ class RfidHid(object):
         if self.dev is None:
             raise ValueError("Device with id %.4x:%.4x not found." % (vendor_id, product_id))
 
+
+    def _hid_set_feature_report(self, report_number, data):
+
+        return self.dev.ctrl_transfer(
+            bmRequestType=self.HID_REQUEST_HOST_TO_DEVICE_CLASS_INTERFACE, 
+            bRequest=self.HID_SET_REPORT, 
+            wValue=self.HID_REPORT_TYPE_FEATURE << 8 | report_number, 
+            wIndex=self.DEVICE_HID_INTERFACE_0, 
+            data_or_wLength=data
+        )
+        
+
+    def _hid_get_feature_report(self, report_number, report_length):
+
+        return self.dev.ctrl_transfer(
+            bmRequestType=self.HID_REQUEST_DEVICE_TO_HOST_CLASS_INTERFACE, 
+            bRequest=self.HID_GET_REPORT, 
+            wValue=self.HID_REPORT_TYPE_FEATURE << 8 | report_number, 
+            wIndex=self.DEVICE_HID_INTERFACE_0, 
+            data_or_wLength=report_length
+        )
+
+
+    @staticmethod
+    def _calculate_crc_sum(payload, init_val=0):
+        r"""Calculate CRC checksum of the payload to be sent to the device.
+        
+        Arguments:
+        payload (list) -- binary representation of the payload as a sequence of bytes.
+        """
+        tmp = init_val
+        for byte in payload:
+            tmp = tmp ^ byte 
+
+        return tmp  
+
+
+    def _get_payload_from_buffer(self, buff, length):
+        return buff[self.STX_POS+1:self.STX_POS+1+length+2]
+
+
     def init(self):
         r"""Initialize the device
 
@@ -53,29 +107,15 @@ class RfidHid(object):
         Issuing a `sudo lsusb -vd vid:pid` should produce the same result.
         """
         desc = usb.control.get_descriptor(
-            self.dev, self.HID_REPORT_DESCRIPTOR_SIZE, 
+            self.dev, 
+            self.DEVICE_HID_REPORT_DESCRIPTOR_SIZE, 
             self.CLASS_TYPE_REPORT, 
-            0)
+            0
+        )
         if not desc:
             raise ValueError("Cannot initialize Device.")
 
         return desc
-
-
-    @staticmethod
-    def __calculate_crc_sum(payload, init_val=CRC_WRITE_INIT_VALUE):
-        r"""Calculate CRC checksum of the payload to be sent to the device.
-        
-        Arguments:
-        payload -- binary representation of Tag's cid + uid as a sequence of bytes.
-                   Example: cid:uid = 77:1234567890 => payload = [0xd4 0x49 0x96 0x02 0xd2]
-        """
-
-        tmp = init_val
-        for byte in payload:
-            tmp = tmp ^ byte 
-
-        return tmp  
 
 
     def beep(self, times=1):
@@ -84,21 +124,21 @@ class RfidHid(object):
         Arguments:
         times -- Number of "beeps" to emit
         """
-
+        payload_length = 3
         buff = [0x00] * self.BUFFER_SIZE
 
         buff[0] = 0x01
         buff[6] = 0x08
-        buff[8] = 0xaa
-        buff[10] = 0x03
-        buff[11] = 0x89
+        buff[8] = self.STX
+        buff[10] = payload_length
+        buff[11] = self.CMD_BEEP
         buff[12] = 0x01
         buff[13] = 0x01
-        buff[14] = 0x8a
-        buff[15] = 0xbb
+        buff[14] = self._calculate_crc_sum(self._get_payload_from_buffer(buff, payload_length))
+        buff[15] = self.ETX
         
         for _ in range(0, times):
-            self.dev.ctrl_transfer(0x21, self.SET_REPORT, 0x0301, 0, buff)
+            self._hid_set_feature_report(1, buff)
             sleep(0.2)
 
 
@@ -107,81 +147,63 @@ class RfidHid(object):
 
         Returns a PayloadResponse object
         """
+        payload_length = 0x03
         buff = [0x00] * self.BUFFER_SIZE
 
         # Setup payload for reading operation
         buff[0x00] = 0x01
         buff[0x06] = 0x08
-        buff[0x08] = 0xaa
-        buff[0x0a] = 0x03
-        buff[0x0b] = 0x25
-        buff[0x0e] = 0x26
-        buff[0x0f] = 0xbb
+        buff[0x08] = self.STX
+        buff[0x0a] = payload_length
+        buff[0x0b] = self.CMD_READ_TAG
+        buff[0x0e] = self._calculate_crc_sum(self._get_payload_from_buffer(buff, payload_length))
+        buff[0x0f] = self.ETX
 
         # Write Feature Report 1
-        response = self.dev.ctrl_transfer(0x21, self.SET_REPORT, 0x0301, 0, buff)
+        response = self._hid_set_feature_report(1, buff)
+
         if response != self.BUFFER_SIZE:
             raise ValueError('Communication Error.')
 
         # Read from Feature Report 2    
-        response = self.dev.ctrl_transfer(0xa1, self.GET_REPORT, 0x0302, 0, self.BUFFER_SIZE).tolist()
+        response = self._hid_get_feature_report(2, self.BUFFER_SIZE).tolist()
 
         return PayloadResponse(response)
 
 
-    def write_tag(self, ids_bytes):
+    def write_tag(self, id_bytes):
         r"""Send a command to "write a tag" 
 
         Arguments:
-        ids_bytes -- Customer ID + UID to be written in binary byte format.
-                     Format: [cid, uid_b3, uid_b2, uid_b1, uid_b0]
+        id_bytes -- Customer ID + UID to be written in binary byte format.
+                    Format: [cid, uid_b3, uid_b2, uid_b1, uid_b0]
         """
-        buff = [0x00] * self.BUFFER_SIZE
-
-        # Setup payload for write operation
-        buff[0x00] = 0x01
-        buff[0x06] = 0x08
-        buff[0x08] = 0xaa
-        buff[0x0a] = 0x03
-        buff[0x0b] = 0x89
-        buff[0x0c] = 0x05
-        buff[0x0d] = 0x01
-        buff[0x0e] = 0x8e
-        buff[0x0f] = 0xbb
-
-        # Write to Feature Report 1
-        response = self.dev.ctrl_transfer(0x21, self.SET_REPORT, 0x0301, 0, buff)
-        if response != self.BUFFER_SIZE:
-            raise ValueError('Communication Error.')
-
-        # Read from Feature Report 2    
-        self.dev.ctrl_transfer(0xa1, self.GET_REPORT, 0x0302, 0, self.BUFFER_SIZE)
-
+        payload_length = 0x1a
         buff = [0x00] * self.BUFFER_SIZE
 
         # Payload containing CID, UID and CRC
         buff[0x00] = 0x01
         buff[0x06] = 0x1f
-        buff[0x08] = 0xaa
-        buff[0x0a] = 0x1a
-        buff[0x0b] = 0x21
+        buff[0x08] = self.STX
+        buff[0x0a] = payload_length
+        buff[0x0b] = self.CMD_WRITE_TAG
         buff[0x0d] = 0x01
         buff[0x0e] = 0x01
         buff[0x0f] = 0x02
-        buff[0x10] = ids_bytes[0] 
-        buff[0x11] = ids_bytes[1]
-        buff[0x12] = ids_bytes[2]
-        buff[0x13] = ids_bytes[3]
-        buff[0x14] = ids_bytes[4]
+        buff[0x10] = id_bytes[0] 
+        buff[0x11] = id_bytes[1]
+        buff[0x12] = id_bytes[2]
+        buff[0x13] = id_bytes[3]
+        buff[0x14] = id_bytes[4]
         buff[0x15] = 0x80
-        buff[0x25] = self.__calculate_crc_sum(ids_bytes)
-        buff[0x26] = 0xbb
+        buff[0x25] = self._calculate_crc_sum(buff[0x09:0x09+payload_length+1])
+        buff[0x26] = self.ETX
 
         # Write to Feature Report 1
-        self.dev.ctrl_transfer(0x21, self.SET_REPORT, 0x0301, 0, buff)
+        self._hid_set_feature_report(1, buff)
 
         # Read from Feature Report 2    
-        self.dev.ctrl_transfer(0xa1, self.GET_REPORT, 0x0302, 0, self.BUFFER_SIZE)
+        return self._hid_get_feature_report(2, self.BUFFER_SIZE)
 
 
     def write_tag_from_cid_and_uid(self, cid, uid):
@@ -202,6 +224,13 @@ class RfidHid(object):
  
         self.write_tag(ids_bytes)
 
+        
+    def _initialize_buffer(self):
+        buff = [0x00] * self.BUFFER_SIZE
+        buff[0x00] = 0x01
+        buff[0x08] = self.STX
+
+        return buff
 
 class PayloadResponse(object):
     r"""Object representation of the response coming from the device"""
