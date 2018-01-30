16ef70c1818afa02e24cc3634e6c03dffc7b0e9f
charlysan <chrlysn0@gmail.com>
2018-01-30 11:18:46 -0300

    Added support for T5577, removed duplicated code

--- rfidhid/core.py
+++ rfidhid/core.py
@@ -35,6 +35,8 @@ class RfidHid(object):
     CMD_READ_TAG = 0x25
     CMD_WRITE_TAG = 0x21
     CMD_BEEP = 0x89
+    TAG_EM4305 = 0x02
+    TAG_T5577 = 0x00
     STX_POS = 0x08
     STX = 0xaa
     ETX = 0xbb
@@ -58,6 +60,125 @@ class RfidHid(object):
         self.dev = usb.core.find(idVendor=vendor_id, idProduct=product_id)
         if self.dev is None:
             raise ValueError("Device with id %.4x:%.4x not found." % (vendor_id, product_id))
+
+
+    def init(self):
+        r"""Initialize the device
+
+        This method should be use to initialize the device in case the OS does not find it.
+        Issuing a `sudo lsusb -vd vid:pid` should produce the same result.
+        """
+        desc = usb.control.get_descriptor(
+            self.dev, 
+            self.DEVICE_HID_REPORT_DESCRIPTOR_SIZE, 
+            self.CLASS_TYPE_REPORT, 
+            0
+        )
+        if not desc:
+            raise ValueError("Cannot initialize Device.")
+
+        return desc
+
+
+    def beep(self, times=1):
+        r"""Send a command to make the device to emit a "beep"
+
+        Arguments:
+        times -- Number of "beeps" to emit
+        """
+        payload_length = 3
+        buff = self._initialize_buffer(payload_length)
+
+        buff[0x06] = 0x08
+        buff[0x0b] = self.CMD_BEEP
+        buff[0x0c] = 0x01
+        buff[0x0d] = 0x01
+        buff[0x0e] = self._calculate_crc_sum(self._get_payload_from_buffer(buff, payload_length))
+        buff[0x0f] = self.ETX
+        
+        for _ in range(0, times):
+            self._hid_set_feature_report(1, buff)
+            sleep(0.2)
+
+
+    def read_tag(self):
+        r"""Send a command to "read a tag" and retrieve the response from the device.
+
+        Returns a PayloadResponse object
+        """
+        payload_length = 0x03
+        buff = self._initialize_buffer(payload_length)
+
+        # Setup payload for reading operation
+        buff[0x06] = 0x08
+        buff[0x0b] = self.CMD_READ_TAG
+        buff[0x0e] = self._calculate_crc_sum(self._get_payload_from_buffer(buff, payload_length))
+        buff[0x0f] = self.ETX
+
+        # Write Feature Report 1
+        response = self._hid_set_feature_report(1, buff)
+
+        if response != self.BUFFER_SIZE:
+            raise ValueError('Communication Error.')
+
+        # Read from Feature Report 2    
+        response = self._hid_get_feature_report(2, self.BUFFER_SIZE).tolist()
+
+        return PayloadResponse(response)
+
+
+    def write_tag(self, id_bytes, tag_type=TAG_EM4305):
+        r"""Send a command to "write a tag" 
+
+        Arguments:
+        id_bytes (list) -- Customer ID + UID to be written in binary byte format.
+                           Format: [cid, uid_b3, uid_b2, uid_b1, uid_b0]
+        tag_type (int)  -- Tag Type (EM4305 or T5577)
+        """
+        payload_length = 0x1a
+        buff = self._initialize_buffer(payload_length)
+
+        # Payload containing CID, UID and CRC
+        buff[0x06] = 0x1f
+        buff[0x0b] = self.CMD_WRITE_TAG
+        buff[0x0d] = 0x01
+        buff[0x0e] = 0x01
+        buff[0x0f] = tag_type
+        buff[0x10] = id_bytes[0] 
+        buff[0x11] = id_bytes[1]
+        buff[0x12] = id_bytes[2]
+        buff[0x13] = id_bytes[3]
+        buff[0x14] = id_bytes[4]
+        buff[0x15] = 0x80
+        buff[0x25] = self._calculate_crc_sum(self._get_payload_from_buffer(buff, payload_length))
+        buff[0x26] = self.ETX
+
+        # Write to Feature Report 1
+        self._hid_set_feature_report(1, buff)
+
+        sleep(0.2)
+
+        # Read from Feature Report 2    
+        return self._hid_get_feature_report(2, self.BUFFER_SIZE)
+
+
+    def write_tag_from_cid_and_uid(self, cid, uid, tag_type=TAG_EM4305):
+        r"""Send a command to "write a tag" 
+
+        Arguments:
+        cid -- (32 bits Integer) Customer ID
+        uid -- (8 bits Integer)  UID
+        """
+        packed_uid = struct.pack('>I', uid)
+
+        if isinstance(packed_uid, str):
+            # python 2.7
+            ids_bytes = [cid] + [ord(x) for x in list(packed_uid)]
+        else:
+            # python 3
+            ids_bytes = [cid] + list(packed_uid)
+ 
+        return self.write_tag(ids_bytes, tag_type)
 
 
     def _hid_set_feature_report(self, report_number, data):
@@ -96,149 +217,28 @@ class RfidHid(object):
         return tmp  
 
 
-    def _get_payload_from_buffer(self, buff, length):
-        return buff[self.STX_POS+1:self.STX_POS+1+length+2]
-
-
-    def init(self):
-        r"""Initialize the device
-
-        This method should be use to initialize the device in case the OS does not find it.
-        Issuing a `sudo lsusb -vd vid:pid` should produce the same result.
-        """
-        desc = usb.control.get_descriptor(
-            self.dev, 
-            self.DEVICE_HID_REPORT_DESCRIPTOR_SIZE, 
-            self.CLASS_TYPE_REPORT, 
-            0
-        )
-        if not desc:
-            raise ValueError("Cannot initialize Device.")
-
-        return desc
-
-
-    def beep(self, times=1):
-        r"""Send a command to make the device to emit a "beep"
-
-        Arguments:
-        times -- Number of "beeps" to emit
-        """
-        payload_length = 3
+    def _initialize_buffer(self, payload_length=0):
         buff = [0x00] * self.BUFFER_SIZE
-
-        buff[0] = 0x01
-        buff[6] = 0x08
-        buff[8] = self.STX
-        buff[10] = payload_length
-        buff[11] = self.CMD_BEEP
-        buff[12] = 0x01
-        buff[13] = 0x01
-        buff[14] = self._calculate_crc_sum(self._get_payload_from_buffer(buff, payload_length))
-        buff[15] = self.ETX
-        
-        for _ in range(0, times):
-            self._hid_set_feature_report(1, buff)
-            sleep(0.2)
-
-
-    def read_tag(self):
-        r"""Send a command to "read a tag" and retrieve the response from the device.
-
-        Returns a PayloadResponse object
-        """
-        payload_length = 0x03
-        buff = [0x00] * self.BUFFER_SIZE
-
-        # Setup payload for reading operation
         buff[0x00] = 0x01
-        buff[0x06] = 0x08
         buff[0x08] = self.STX
         buff[0x0a] = payload_length
-        buff[0x0b] = self.CMD_READ_TAG
-        buff[0x0e] = self._calculate_crc_sum(self._get_payload_from_buffer(buff, payload_length))
-        buff[0x0f] = self.ETX
-
-        # Write Feature Report 1
-        response = self._hid_set_feature_report(1, buff)
-
-        if response != self.BUFFER_SIZE:
-            raise ValueError('Communication Error.')
-
-        # Read from Feature Report 2    
-        response = self._hid_get_feature_report(2, self.BUFFER_SIZE).tolist()
-
-        return PayloadResponse(response)
-
-
-    def write_tag(self, id_bytes):
-        r"""Send a command to "write a tag" 
-
-        Arguments:
-        id_bytes -- Customer ID + UID to be written in binary byte format.
-                    Format: [cid, uid_b3, uid_b2, uid_b1, uid_b0]
-        """
-        payload_length = 0x1a
-        buff = [0x00] * self.BUFFER_SIZE
-
-        # Payload containing CID, UID and CRC
-        buff[0x00] = 0x01
-        buff[0x06] = 0x1f
-        buff[0x08] = self.STX
-        buff[0x0a] = payload_length
-        buff[0x0b] = self.CMD_WRITE_TAG
-        buff[0x0d] = 0x01
-        buff[0x0e] = 0x01
-        buff[0x0f] = 0x02
-        buff[0x10] = id_bytes[0] 
-        buff[0x11] = id_bytes[1]
-        buff[0x12] = id_bytes[2]
-        buff[0x13] = id_bytes[3]
-        buff[0x14] = id_bytes[4]
-        buff[0x15] = 0x80
-        buff[0x25] = self._calculate_crc_sum(buff[0x09:0x09+payload_length+1])
-        buff[0x26] = self.ETX
-
-        # Write to Feature Report 1
-        self._hid_set_feature_report(1, buff)
-
-        # Read from Feature Report 2    
-        return self._hid_get_feature_report(2, self.BUFFER_SIZE)
-
-
-    def write_tag_from_cid_and_uid(self, cid, uid):
-        r"""Send a command to "write a tag" 
-
-        Arguments:
-        cid -- (32 bits Integer) Customer ID
-        uid -- (8 bits Integer)  UID
-        """
-        packed_uid = struct.pack('>I', uid)
-
-        if isinstance(packed_uid, str):
-            # python 2.7
-            ids_bytes = [cid] + [ord(x) for x in list(packed_uid)]
-        else:
-            # python 3
-            ids_bytes = [cid] + list(packed_uid)
- 
-        self.write_tag(ids_bytes)
-
-        
-    def _initialize_buffer(self):
-        buff = [0x00] * self.BUFFER_SIZE
-        buff[0x00] = 0x01
-        buff[0x08] = self.STX
 
         return buff
 
+
+    def _get_payload_from_buffer(self, buff, length):
+        r"""Extract payload from buffer based on STX position and payload length"""
+
+        return buff[self.STX_POS+1:self.STX_POS+1+length+2]
+
+
 class PayloadResponse(object):
     r"""Object representation of the response coming from the device"""
-    RESPONSE_LENGTH_WITH_TAG = 19
-    CID_POS = 12
-    UID_MSB_POS = 13
-    UID_LSB_POS = 16
-    CRC_READ_POS = 17
+    RESPONSE_LENGTH_WITH_TAG = 0x13
+    CID_POS = 0x0c
+    UID_MSB_POS = 0x0d
+    UID_LSB_POS = 0x10
+    CRC_READ_POS = 0x11
 
     def __init__(self, data):
         self.data = data
@@ -250,6 +250,7 @@ class PayloadResponse(object):
             self.cid = self.data[self.CID_POS]
             self.uid = self.data[self.UID_MSB_POS:self.UID_LSB_POS+1]
             self.crc = self.data[self.CRC_READ_POS]
+
 
     def get_tag_uid_as_byte_sequence(self):
         r"""Gets the Tag's UID as a sequence of bytes. E.g. [0x23, 0xa4, 0x23, 0x56]"""
