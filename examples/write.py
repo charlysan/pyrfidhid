cc4ea82e16d968a1b2ab791ab51f759ce5be3b05
charlysan <carlos.masciarelli@bitlogic.io>
2019-03-10 23:27:42 -0300

    Add license note

--- examples/write.py
+++ examples/write.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 charlysan
+# Copyright (c) 2019 charlysan
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
