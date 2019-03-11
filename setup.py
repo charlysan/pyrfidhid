b492f0b915c30e74f3d645c199121a8fa013c553
charlysan <carlos.masciarelli@bitlogic.io>
2019-03-11 00:17:56 -0300

    Add entry point to setuptools

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ setup(name='pyrfidhid',
       version='0.2',
       description='Library to control 125Khz RFID USB HID Device',
       url='https://github.com/charlysan/pyrfidhid/',
+      # scripts=['cli/rfid_cli.py'],
+      entry_points={
+        'console_scripts': [
+            'rfid_cli = cli.rfid_cli:main'
+        ]
+      },
       author='charlysan',
       author_email='chrlysn0@gmail.com',
       license='MIT',
